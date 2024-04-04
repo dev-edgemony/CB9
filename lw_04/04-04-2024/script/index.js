7ada55be671fc79acb1bbb81c16f645aa87fc9df
robertafavuzza <roberta.favuzza98@gmail.com>
2024-04-04 23:56:09 +0200

    first JavaScript exercise

---
+++ lw_04/04-04-2024/script/index.js
@@ -0,0 +1,14 @@
+//Primo Esercizio 🎁
+let currentDate = '04/04/2024';
+let birthday = '30/04/1998';
+
+
+
+if (currentDate === birthday) {
+
+  console.log('🎁');
+} else {
+
+  console.log('Oggi non è il tuo compleanno.');
+}
+
