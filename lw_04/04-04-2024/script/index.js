d632c1d6e37b7deb1a89485262fbf5b6114a342a
robertafavuzza <roberta.favuzza98@gmail.com>
2024-04-05 00:08:23 +0200

    second JavaScript exercise

--- lw_04/04-04-2024/script/index.js
+++ lw_04/04-04-2024/script/index.js
@@ -1,14 +1,34 @@
 //Primo Esercizio 🎁
-let currentDate = '04/04/2024';
-let birthday = '30/04/1998';
+//let currentDate = '04/04/2024';
+//let birthday = '30/04/1998';
 
 
 
-if (currentDate === birthday) {
+//if (currentDate === birthday) {
 
-  console.log('🎁');
-} else {
+  //console.log('🎁');
+//} else {
 
-  console.log('Oggi non è il tuo compleanno.');
-}
+ // console.log('Oggi non è il tuo compleanno.');
+//}
+
+
+//Secondo Esercizio
+
+const dailySteps = [
+    'mi sveglio',
+    'faccio colazione',
+    'faccio esercizi',
+    'faccio la doccia',
+    'porto a spasso il cane',
+    'pranzo',
+    'lavoro/studio',
+    'ceno',
+    'gioco al PC',
+    'vado a letto'
+  ];
+
+  for (let i = 0; i < dailySteps.length; i++) {
+    console.log(dailySteps[i]);
+  }
 
