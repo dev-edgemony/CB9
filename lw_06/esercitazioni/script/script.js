164b0cdb27ae91c001b32b105bde7d40ba9a3caa
robertafavuzza <roberta.favuzza98@gmail.com>
2024-04-17 21:46:40 +0200

    Exercise 2 - Week 6

--- lw_06/esercitazioni/script/script.js
+++ lw_06/esercitazioni/script/script.js
@@ -1,6 +1,6 @@
 //## 1. Calcolo della Media
 
-function mediaOp(arrNums) {
+/* function mediaOp(arrNums) {
     if (arrNums.length === 0) {
         return 0; 
     }
@@ -19,4 +19,17 @@ function mediaOp(arrNums) {
 
 const arrNums = [10, 15, 23, 45, 80];
 let mediaResult = mediaOp(arrNums);
-console.log('La media è:', mediaResult);
+console.log('La media è:', mediaResult); */
+
+//## 2. Generatore di Immagine
+
+function ImgGenerator(src, alt) {   
+    const imgEl = document.createElement('img');
+    imgEl.src = src;
+    imgEl.alt = alt;
+    return imgEl;
+}
+
+    const containerEl = document.getElementById('container');
+    const newImage = ImgGenerator('https://picsum.photos/200/300');
+    containerEl.appendChild(newImage);
