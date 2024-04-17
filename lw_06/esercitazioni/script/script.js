3f616554d8ca4dacbd41dfacfc2c567bbe1121a5
robertafavuzza <roberta.favuzza98@gmail.com>
2024-04-17 21:08:23 +0200

    Exercise 1 - Week 6

--- lw_06/esercitazioni/script/script.js
+++ lw_06/esercitazioni/script/script.js
@@ -1,1 +1,22 @@
+//## 1. Calcolo della Media
 
+function mediaOp(arrNums) {
+    if (arrNums.length === 0) {
+        return 0; 
+    }
+
+    let somma = 0;
+
+    for (let i = 0; i < arrNums.length; i++) {
+        somma += arrNums[i];
+    }
+
+
+    const media = somma / arrNums.length;
+
+    return media;
+}
+
+const arrNums = [10, 15, 23, 45, 80];
+let mediaResult = mediaOp(arrNums);
+console.log('La media è:', mediaResult);
