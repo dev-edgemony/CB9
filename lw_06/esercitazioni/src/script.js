35a1545d84b8c2c5675b557000864ad12808576a
cacky2112 <carolina.serra90@gmail.com>
2024-04-19 21:09:35 +0200

    f: aggiornamento es4 lw6

--- lw_06/esercitazioni/src/script.js
+++ lw_06/esercitazioni/src/script.js
@@ -43,3 +43,82 @@ colorChange.addEventListener("click", (event) => {
     colorChange.style.backgroundColor = '#e43939';
 
 });
+
+// Esercizio 4
+const cardWrapperEl = document.querySelector(".card-wrapper");
+const cardGen = (data) => {
+
+    const cardEl = document.createElement("li");
+    const cardName = document.createElement("h3");
+    const cardDay = document.createElement("h4");
+    const cardIcon = document.createElement("img");
+    const cardTemp = document.createElement("p");
+    const cardWeather = document.createElement("p");
+
+    cardEl.className = "card";
+    cardName.textContent = data.name;
+    cardDay.textContent = data.day;
+    cardIcon.src = data.icon;
+    cardTemp.textContent = data.temperature;
+    cardWeather.textContent = data.weather;
+    
+    cardEl.append(cardName, cardDay, cardIcon, cardTemp, cardWeather);
+    return cardEl;
+};
+
+const cardListGen = () => {
+    const cardEl = document.createElement("ul");
+    cardEl.className = "list";
+  
+    return cardEl;
+};
+
+const weatherCard = [
+    {
+    name: "Ferrara",
+    day: "19/04/2024 12:00",
+    icon: "media/sunny.svg",
+    temperature: "20°C",
+    weather: "Soleggiato"
+    },
+    {
+    name: "Ravenna",
+    day: "19/04/2024 12:00",
+    icon: "media/cloudy.svg",
+    temperature: "19°C",
+    weather: "Nuvoloso"
+    },
+    {
+    name: "Bologna",
+    day: "19/04/2024 12:00",
+    icon: "media/partially-cloud.svg",
+    temperature: "22°C",
+    weather: "Soleggiato con nuvole sparse"
+    },
+    {
+    name: "Forlì",
+    day: "19/04/2024 12:00",
+    icon: "media/rainy.svg",
+    temperature: "21°C",
+    weather: "Piovoso"
+    },
+    {
+    name: "Cesena",
+    day: "19/04/2024 12:00",
+    icon: "media/windy.svg",
+    temperature: "18°C",
+    weather: "Vento forte"
+    },
+    // {
+    // name: "Rimini",
+    // day: "19/04/2024 12:00",
+    // icon: "media/rainy.svg",
+    // temperature: "18°C",
+    // weather: "Piovoso"
+    // }
+]
+
+const cardList = cardListGen();
+weatherCard.map((card) => cardList.append(cardGen(card)));
+
+cardWrapperEl.append(cardList);
