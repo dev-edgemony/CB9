ab02bc0eec039efb8a24d5e79b5cec1d53ce5076
cacky2112 <carolina.serra90@gmail.com>
2024-04-19 14:08:49 +0200

    Aggiornamento es2 lw06

--- lw_06/esercitazioni/src/script.js
+++ lw_06/esercitazioni/src/script.js
@@ -13,14 +13,18 @@ console.log("La media dell'array è di: " + calculateAverage(arr));
 
 
 // Esercizio 2
-const imgGen = (image) => {
-    const image = document.createElement("img");
-  
-    image.className = "img-js";
-    image.src = foto.jpg;
-    image.alt = "Immagine creata con Javascript";
-  
-    return image;
+
+const es2El = document.querySelector(".es2");
+const imgGen = (src, alt) => {
+    const imgEl = document.createElement("img");
+    imgEl.className = "img";
+    imgEl.src = src;
+    imgEl.alt = alt;
+    
+    return imgEl;
 };
 
-body.append(imgGen);
+const image1 = imgGen("https://picsum.photos/200/300?1", "Immagine 1 creata con Javascript");
+const image2 = imgGen("https://picsum.photos/200/300?2", "Immagine 2 creata con Javascript");
+
+es2El.append(image1, image2);
