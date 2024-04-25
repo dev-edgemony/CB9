32944fb4687345fb09415382084aa9efd2812ba6
cacky2112 <carolina.serra90@gmail.com>
2024-04-25 17:56:35 +0200

    f: es 1 lw7 - up 1

--- lw_07/esercitazioni/src/script.js
+++ lw_07/esercitazioni/src/script.js
@@ -3,7 +3,7 @@
 const genImageCardEl = (imageData) => {
     const imageEl = document.createElement("img")
 
-    imageEl.className = "image-card";
+    imageEl.className = "card-image";
     imageEl.src = imageData.urls.small;
     imageEl.alt = imageData.alt_description;
 
@@ -11,13 +11,32 @@ const genImageCardEl = (imageData) => {
 }
 
 const genImageListEl = (listName) => {
-    const wrapperEl = document
+    const wrapperEl = document.createElement("div");
+    const titleEl = document.createElement("h4");
+    
+    wrapperEl.className = "card-list";
+    titleEl.textContent = listName;
+
+    wrapperEl.append(titleEl);
+    return wrapperEl;
 }
 
 const BASE_URL = "https://api.unsplash.com/";
 const API_TOKEN = "8wKit5s2VjUlIsi0WtYMPbJft8GeEiek6nORNm07quY";
-const SEARCH_QUERY = 'dog';
+const SEARCH_QUERY = 'baby';
+const rootEl = document.querySelector(".root");
 
 fetch(`${BASE_URL}/search/photos/?client_id=${API_TOKEN}&query=${SEARCH_QUERY}`)
     .then((res) => res.json())
-    .then((data) => console.log(data.results[0].urls.small));
+    .then((data) => {
+        const listData = data.results;
+        const imageListEl = genImageListEl("The photos");
+        
+        listData.map(photo => {
+            const imgCardEl = genImageCardEl(photo);
+
+            imageListEl.append(imgCardEl)
+        })
+        
+        rootEl.append(imageListEl);
+    });
