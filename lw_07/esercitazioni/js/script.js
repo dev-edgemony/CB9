6f5aae4e70d8ad36d0ca7ddccfa16811f1436efe
GiuseppeSonny <umanella23@gmail.com>
2024-04-29 00:09:03 +0200

    new javascript exercise

---
+++ lw_07/esercitazioni/js/script.js
@@ -0,0 +1,52 @@
+//-----ESERCITAZIONE Unsplash e selettore immagini----////
+
+ const imgGen = (imgData) => {
+  const wrapperEl = document.createElement("div");
+  const imgEl = document.createElement("img");
+
+  wrapperEl.className = "img--wrapper";
+  imgEl.className = "imgEl";
+  imgEl.src = imgData.urls.small;
+  imgEl.alt = imgData.alt_description;
+
+  wrapperEl.addEventListener("click", () => {
+    window.open(imgData.urls.full, "_blank");
+  });
+
+  wrapperEl.append(imgEl);
+  return wrapperEl;
+};
+
+const ImgListEl = (imgList) => {
+  const wrapperList = document.createElement("div");
+  const listTitle = document.createElement("h3");
+  
+  wrapperList.className = "imgList";
+  listTitle.textContent = imgList;
+
+  wrapperList.append(listTitle);
+  return wrapperList;
+};
+
+const BASE_URL = "https://api.unsplash.com";
+const API_TOKEN = "usEey9f7mX9cID_VGyfjztK8YeIYTKaA5CRB8aQjgiY";
+const SEARCH_QUERY = "Tattoo";
+const PAGE = 2;
+const PER_PAGE = 20;
+const ROOT_EL = document.querySelector(".root");
+
+fetch(
+  `${BASE_URL}/search/photos/?client_id=${API_TOKEN}&query=${SEARCH_QUERY}&per_page=${PER_PAGE}&page=${PAGE}`
+)
+
+.then((res) => res.json())
+.then((data) => {
+  const wrapperList = ImgListEl("Tattoo Images");
+   
+    data.results.forEach((photo) => {
+    wrapperList.append(imgGen(photo));
+
+  });
+
+  ROOT_EL.append(wrapperList);
+ });
