743ddcf4ccdcd6308e9e191865e571c2299dbd4c
cacky2112 <carolina.serra90@gmail.com>
2024-05-15 23:12:12 +0200

    aggiornamento code week

---
+++ lw_10/code-week/js/components/Helpers.js
@@ -0,0 +1,7 @@
+export const createEl = (el) => document.createElement(el)
+
+export const find = (selector) => document.querySelector(selector)
+
+export const log = (data) => console.log(data)
+
+export const nul = () => {}
